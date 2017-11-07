0932be1a6057a5325c20a343a182e1088088cb12
paul <paul604@users.noreply.github.com>
2017-11-07 15:42:41 +0100

    pb Philosophe OK

--- src/prog2/concur/exercice1/Philosophe.java
+++ src/prog2/concur/exercice1/Philosophe.java
@@ -1,18 +1,19 @@
 package prog2.concur.exercice1;
 
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 class Baguette {
     private boolean _prise = false;
 
-    final synchronized void prendre() {
-        try {
-            while (_prise) {
-                wait();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            System.exit(-1);
+    final synchronized boolean prendreBool() {
+        if (_prise) {
+            return false;
         }
         _prise = true;
+        return true;
     }
 
     final synchronized void relacher() {
@@ -21,33 +22,59 @@ class Baguette {
     }
 }
 
+class GestionBaguettes{
+
+    public synchronized void prendre(Baguette baguette1, Baguette baguette2){
+        if (! (baguette1.prendreBool() || baguette2.prendreBool())){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }else{
+            //évite les problèmes de deadLock si le philosophe a pris une des deux baguettes
+            relacher(baguette1, baguette2);
+        }
+    }
+
+    public synchronized void relacher(Baguette baguette1, Baguette baguette2){
+        baguette1.relacher();
+        baguette2.relacher();
+    }
+}
+
 public class Philosophe implements Runnable {
     private String _nom;
     private Baguette _bGauche, _bDroite;
+    private static GestionBaguettes gestionBaguettes = new GestionBaguettes();
+    private int nbPenser;
+    private int nbManger;
 
     public Philosophe(String n, Baguette g, Baguette d) {
         _nom = n;
         _bGauche = g;
         _bDroite = d;
+        nbPenser = 0;
+        nbManger = 0;
     }
 
     public void run() {
         while (true) {
             penser();
-            _bGauche.prendre();
-            _bDroite.prendre();
+            gestionBaguettes.prendre(_bDroite, _bGauche);
             manger();
-            _bDroite.relacher();
-            _bGauche.relacher();
+            gestionBaguettes.relacher(_bDroite, _bGauche);
         }
     }
 
     final void manger() {
-        System.out.println(_nom + " mange.");
+//        System.out.println(_nom + " mange.");
+        nbManger++;
     }
 
     final void penser() {
-        System.out.println(_nom + " pense.");
+//        System.out.println(_nom + " pense.");
+        nbPenser++;
     }
 
     public static void main(String args[]) {
@@ -56,10 +83,34 @@ public class Philosophe implements Runnable {
         Philosophe[] table;
 
         table = new Philosophe[5];
+        //utilisation d'un ExecutorService pour pouvoir stopper et attendre tout les threads
+        ExecutorService es = Executors.newCachedThreadPool();
         for (char cpt = 0; cpt < table.length; ++cpt) {
             table[cpt] = new Philosophe(noms[cpt], baguettes[cpt], baguettes[(cpt + 1) % table.length]);
-            new Thread(table[cpt]).start();
+            es.execute(new Thread(table[cpt]));//run thread
+        }
+        try {
+            System.out.println("Pour finir appuyé sur une touche");
+            Scanner scanner = new Scanner(System.in);
+            scanner.nextLine();
+            scanner.close();
+            es.shutdown();//stop les threads
+
+            //attente de tous les threads
+            es.awaitTermination(1, TimeUnit.SECONDS);
+            //affiche les informations des philosophes
+            System.out.println("stats");
+            System.out.println("-----------------------------------");
+            for (Philosophe philosophe : table) {
+                System.out.println(philosophe._nom);
+                System.out.println("    nbManger: " + philosophe.nbManger);
+                System.out.println("    nbPenser: " + philosophe.nbPenser);
+                System.out.println("    total: " + (philosophe.nbPenser+philosophe.nbManger));
+                System.out.println("-----------------------------------");
+            }
+            System.exit(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
-
