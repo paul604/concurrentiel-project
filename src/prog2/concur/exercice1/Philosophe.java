62b405955e86ae20e70f7f99aadeb9bc80db816b
paul <paul604@users.noreply.github.com>
2017-11-09 15:06:58 +0100

    up import in Philosophe

--- src/prog2/concur/exercice1/Philosophe.java
+++ src/prog2/concur/exercice1/Philosophe.java
@@ -1,7 +1,5 @@
 package prog2.concur.exercice1;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
