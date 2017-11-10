ca7f0843afd38f947811cfe16afb6501912fb69f
paul <paul604@users.noreply.github.com>
2017-11-10 14:54:03 +0100

    File2

--- src/prog2/concur/exercice2/File2.java
+++ src/prog2/concur/exercice2/File2.java
@@ -1,21 +1,62 @@
 package prog2.concur.exercice2;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * haut niveau
  */
 public class File2<T> extends AbstractFileBloquanteBornee<T> {
 
+    private ReadWriteLock readWriteLock;
+
     public File2(int n) throws IllegalArgumentException {
         super(n);
+        readWriteLock = new ReentrantReadWriteLock();
     }
 
     @Override
-    public void deposer(T s) throws InterruptedException {
+    public synchronized void deposer(T s) throws InterruptedException {
+        System.out.println("deposer");
+        readWriteLock.writeLock().lock();
+        while ( super.estPleine){
+            wait();
+        }
 
+        super.tableau[super.queue]=s;
+        super.queue++;
+
+        if (super.queue>=super.tableau.length) {
+            super.estPleine=true;
+        }
+        super.estVide=false;
+
+        notify();
+        readWriteLock.writeLock().unlock();
     }
 
     @Override
-    public T prendre() throws InterruptedException {
-        return null;
+    public synchronized T prendre() throws InterruptedException {
+        System.out.println("prendre");
+        readWriteLock.readLock().lock();
+        while (super.estVide){
+            wait();
+        }
+
+        T out = super.tableau[super.tete];
+
+        super.queue--;
+        for (int i = 0; i < super.queue; i++) {
+            super.tableau[i]=super.tableau[i+1];
+        }
+
+        if (super.queue<=0) {
+            super.estVide=true;
+        }
+        super.estPleine=false;
+
+        notify();
+        readWriteLock.readLock().unlock();
+        return out;
     }
 }
